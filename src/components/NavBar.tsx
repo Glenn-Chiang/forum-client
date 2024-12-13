2d8cbdd8dfea192b575e40592f71a66c7811d1bf
Glenn <glennchiang24@gmail.com>
2024-12-13 16:16:04 +0800

    Add NavBar links to Home and Login

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,28 @@
-import { AppBar, Box, Button, IconButton, Toolbar, Typography } from "@mui/material";
-import MenuIcon from '@mui/icons-material/Menu'
+import MenuIcon from "@mui/icons-material/Menu";
+import { AppBar, Box, Button, IconButton, Link, Toolbar } from "@mui/material";
+import { Link as RouterLink } from "react-router";
 
 export default function NavBar() {
   return (
-    <Box sx={{flexGrow: 1}}>
+    <Box sx={{ flexGrow: 1 }}>
       <AppBar position="sticky">
         <Toolbar>
-          <IconButton size="large" edge="start" color="inherit" sx={{ mr: 2}}>
-            <MenuIcon/>
+          <IconButton size="large" edge="start" color="inherit" sx={{ mr: 2 }}>
+            <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1}}>
+          <Link
+            component={RouterLink}
+            to={"/"}
+            variant="h6"
+            color="inherit"
+            underline="none"
+            sx={{ flexGrow: 1 }}
+          >
             Forum
-          </Typography>
-          <Button color="inherit">Login</Button>
+          </Link>
+          <Button component={RouterLink} to="/login" color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </Box>
-  )
+  );
 }
