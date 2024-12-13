41ab61d29854db855bb6fb80490695bedcad8883
Glenn <glennchiang24@gmail.com>
2024-12-13 22:34:31 +0800

    Create CreatePostPage

--- src/pages/CreatePostPage.tsx
+++ src/pages/CreatePostPage.tsx
@@ -1,3 +1,18 @@
+import { Box, Button, TextField, Typography } from "@mui/material";
+
 export default function CreatePostPage() {
-  return <></>
+  return (
+    <Box padding={1} display="flex" flexDirection="column" gap={1}>
+      <Typography variant="h6">Create a Post</Typography>
+      <TextField fullWidth placeholder="Title" />
+      <TextField
+        fullWidth
+        placeholder="Content"
+        multiline
+        rows={5}
+        maxRows={20}
+      />
+      <Button sx={{maxWidth: 100}} variant="contained">Post</Button>
+    </Box>
+  );
 }
