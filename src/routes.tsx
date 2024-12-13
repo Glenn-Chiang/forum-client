b6450d0e855f9bee4164a9ebb40f22b9c1e2f097
Glenn <glennchiang24@gmail.com>
2024-12-13 15:40:48 +0800

    Set up material UI

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,27 +7,30 @@ import PostPage from "./pages/PostPage";
 import CreatePostPage from "./pages/CreatePostPage";
 import EditPostPage from "./pages/EditPostPage";
 import ProfilePage from "./pages/ProfilePage";
+import BaseLayout from "./pages/BaseLayout";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Home />} />
+        <Route element={<BaseLayout/>}>
+          <Route index element={<Home />} />
 
+          <Route path="posts">
+            <Route index element={<Home />} />
+            <Route path=":id" element={<PostPage/>}/>
+            <Route path="create" element={<CreatePostPage/>}/>
+            <Route path="edit" element={<EditPostPage/>}/>
+          </Route>
+
+          <Route path="profiles">
+            <Route path=":id" element={<ProfilePage/>}/>
+          </Route>
+        </Route>
+        
         <Route element={<AuthLayout/>}>
           <Route path="login" element={<LoginPage/>}/>
           <Route path="register" element={<RegisterPage/>}/>
-        </Route>
-
-        <Route path="posts">
-          <Route index element={<Home />} />
-          <Route path=":id" element={<PostPage/>}/>
-          <Route path="create" element={<CreatePostPage/>}/>
-          <Route path="edit" element={<EditPostPage/>}/>
-        </Route>
-
-        <Route path="profiles">
-          <Route path=":id" element={<ProfilePage/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
