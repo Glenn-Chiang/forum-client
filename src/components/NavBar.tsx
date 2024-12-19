35b4fafbe38e2fdfad78d8785e2251c532dc2da7
Glenn <glennchiang24@gmail.com>
2024-12-19 17:52:24 +0800

    Modify NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,36 +1,45 @@
-import { AppBar, Button, Link, Toolbar } from "@mui/material";
+import { Add } from "@mui/icons-material";
+import { AppBar, Button, Link, Stack, Toolbar } from "@mui/material";
 import { Link as RouterLink } from "react-router";
 import { selectCurrentUserId } from "../auth/authSlice";
 import { useAppSelector } from "../store";
 import AccountMenu from "./AccountMenu";
-import NavMenu from "./NavMenu";
 
 export default function NavBar() {
   const userId = useAppSelector(selectCurrentUserId);
   const authenticated = userId !== null;
 
   return (
-      <AppBar position="sticky">
-        <Toolbar>
-          <NavMenu />
-          <Link
-            component={RouterLink}
-            to={"/"}
-            variant="h6"
-            color="inherit"
-            underline="none"
-            sx={{ flexGrow: 1 }}
-          >
-            Forum
-          </Link>
-          {authenticated ? (
-            <AccountMenu />
-          ) : (
-            <Button component={RouterLink} to="/login" color="inherit">
-              Login
+    <AppBar position="sticky">
+      <Toolbar>
+        <Link
+          component={RouterLink}
+          to={"/"}
+          variant="h6"
+          color="inherit"
+          underline="none"
+          sx={{ flexGrow: 1 }}
+        >
+          Forum
+        </Link>
+        {authenticated ? (
+          <Stack direction={"row"} spacing={2}>
+            <Button
+              component={RouterLink}
+              to="/posts/create"
+              color="inherit"
+              startIcon={<Add />}
+            >
+              Create post
             </Button>
-          )}
-        </Toolbar>
-      </AppBar>
+            <AccountMenu />
+          </Stack>
+        ) : (
+          <Button component={RouterLink} to="/login" color="inherit">
+            Login
+          </Button>
+        )}
+      </Toolbar>
+    </AppBar>
   );
 }
