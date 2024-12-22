2836ee6fe267890f55ab79aa461f7ad6a9ce587f
Glenn <glennchiang24@gmail.com>
2024-12-22 16:24:18 +0800

    Change BackButton to navigate to Home

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,11 +1,10 @@
 import { ArrowBack } from "@mui/icons-material";
 import { IconButton } from "@mui/material";
-import { useNavigate } from "react-router";
+import { Link } from "react-router";
 
 export default function BackButton() {
-  const navigate = useNavigate();
   return (
-    <IconButton onClick={() => navigate(-1)}>
+    <IconButton component={Link} to="/">
       <ArrowBack />
     </IconButton>
   );
